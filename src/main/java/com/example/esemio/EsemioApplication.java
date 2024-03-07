63207ff5e56ba44d0c2259e941fd3bd98e24a447
francescosabatino <francesco.sabatino1997@gmail.com>
2024-03-07 17:29:31 +0100

    Initial commit

---
+++ src/main/java/com/example/esemio/EsemioApplication.java
@@ -0,0 +1,13 @@
+package com.example.esemio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsemioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsemioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/esemio/EsemioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esemio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsemioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
